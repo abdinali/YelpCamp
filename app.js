9e591055373164a44334cf21d2c4d6690932bd07
Abdinasir Ali <131626624+abdinasir5@users.noreply.github.com>
2024-05-15 22:00:36 -0400

    Updated styles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -14,8 +15,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/yelpcamp')
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.engine('ejs', ejsMate);
 app.use(express.urlencoded({extended: true}));
-app.use(methodOverride('_method'))
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
