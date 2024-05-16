04897b3c7b9f4996474d15dd903e3460e09f0fc7
Abdinasir Ali <131626624+abdinasir5@users.noreply.github.com>
2024-05-15 22:59:12 -0400

    Updated campgrounds model for images

--- seeds/index.js
+++ seeds/index.js
@@ -13,9 +13,17 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const randomLocation = sample(cities); 
+        const change = [0.49, 0.79, 0.95, 0.99];
+        const price = (Math.floor(Math.random() * 30) + 10) + change[Math.floor(Math.random() * change.length)];
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
-            location: `${randomLocation.city}, ${randomLocation.state}`
+            image: 'https://source.unsplash.com/collection/483251',
+            price: price,
+            description: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                          Ab totam eaque dolor ipsum ipsa natus architecto eos doloribus 
+                          vero beatae, temporibus aliquid atque odio maxime fugit sint 
+                          distinctio minima eius.`,
+            location: `${randomLocation.city}, ${randomLocation.state}`,
         })
         await camp.save();
     }
